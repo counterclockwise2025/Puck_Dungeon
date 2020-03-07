ad656f7254f20e4ff367f227db31a190116eac8a
Samip Vaidh <svd246@gmail.com>
2020-03-07 18:42:04 -0500

    Abstract Spell Class
    Create a generalized spell class that can be later defined that way new spells can easily be implemented in the future if need be.

---
+++ Puck Dungeon/Assets/Scripts/spellsAbstract.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//creating an abstract spell class inorder to make the spells modular that way we can add more spells easily in the future
+public abstract class spellsAbstract : MonoBehaviour
+{
+    public GameObject aimArrow;
+    public GameObject arrowPuckPrefab;
+    playerScript pScript;
+    gameControllerScriptNew gcScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
