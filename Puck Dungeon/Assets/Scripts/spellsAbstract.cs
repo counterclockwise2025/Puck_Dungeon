cf3bd94a4d8f531a58f76c2fb3e907adbe057e2d
Samip Vaidh <svd246@gmail.com>
2020-03-08 15:00:15 -0400

    Update spellsAbstract.cs
    Add in a constructor for generalized attibutes that pretrain to a spell, may need to be improved upon more as the project progress

--- Puck Dungeon/Assets/Scripts/spellsAbstract.cs
+++ Puck Dungeon/Assets/Scripts/spellsAbstract.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 //creating an abstract spell class inorder to make the spells modular that way we can add more spells easily in the future
 public abstract class spellsAbstract : MonoBehaviour
 {
+    public class Spell
+    {
+        int spellShoot;
+        int spellCount;
+
+        public Spell(int shoot, int count)
+        {
+            spellShoot = shoot;
+            spellCount = count;
+        }
+    }
+
+    public Spell playerSpell = new Spell(5,5);
     public GameObject aimArrow;
     public GameObject arrowPuckPrefab;
     playerScript pScript;
